d03fe4f28cdd5e6fe71b523b95fd8f0a1f507516
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-06-14 01:33:41 +0300

    Create Ashq-bdwy.com.js

---
+++ Ashq-bdwy.com.js
@@ -0,0 +1,482 @@
+//if(location.protocol!='https:'){location.protocol='https:';}
+
+ 
+
+$("img[src='prv1.png?2']").attr("src","https://cf1.s3.souqcdn.com/item/2017/01/24/17/52/15/27/item_XL_17521527_23879918.jpg");
+
+ 
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · мυsɪc · ●&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>اغاني</p><audio controls loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+
+ 
+
+$(`<img src ="https://ashq-bdwy.com/ashq-bdwy.gif" etclass="fr" style="width:100%;margin-top: 1px;"><div id="asim10"><center><a id="asim33" href="#" target="_blank" type="button" style="border: 9px double #eb0c3c;
+
+ 
+
+border-radius: 98px 0;
+
+ 
+
+background-image: -webkit-gradient(linear, center top, center bottom, from(#d1ede9), to(#00bfa2), color-stop(3%, #c1e9e3), color-stop(12%, #b1e4dc), color-stop(90%, #59d2bf));
+
+ 
+
+background-image: -webkit-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -moz-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -o-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -ms-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: linear-gradient(to bottom, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+-webkit-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+-moz-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96); width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="#" target="_blank" type="button" style="border: 9px double #eb0c3c;
+
+ 
+
+border-radius: 98px 0; width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اختصارات</a><a id="asim33" href="#" target="_blank" type="button" style="border: 9px double #eb0c3c;
+
+ 
+
+border-radius: 98px 0;
+
+ 
+
+background-image: -webkit-gradient(linear, center top, center bottom, from(#d1ede9), to(#00bfa2), color-stop(3%, #c1e9e3), color-stop(12%, #b1e4dc), color-stop(90%, #59d2bf));
+
+ 
+
+background-image: -webkit-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -moz-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -o-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: -ms-linear-gradient(top, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+background-image: linear-gradient(to bottom, #d1ede9, #c1e9e3 3%, #b1e4dc 12%, #59d2bf 90%, #00bfa2);
+
+ 
+
+-webkit-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+-moz-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96); width: 22%; margin: 2px;" class="btn btn-primary btn-sm">زخرفة</a><a id="asim33" href="#" target="_blank" type="button" style="border: 9px double #eb0c3c;
+
+ 
+
+border-radius: 98px 0;
+
+ 
+
+background-image: -webkit-gradient(linear, center top, center bottom, from(#f2ab2b), to(#e17b19));
+
+ 
+
+background-image: -webkit-linear-gradient(top, #f2ab2b, #e17b19);
+
+ 
+
+background-image: -moz-linear-gradient(top, #f2ab2b, #e17b19);
+
+ 
+
+background-image: -o-linear-gradient(top, #f2ab2b, #e17b19);
+
+ 
+
+background-image: -ms-linear-gradient(top, #f2ab2b, #e17b19);
+
+ 
+
+background-image: linear-gradient(to bottom, #f2ab2b, #e17b19);
+
+ 
+
+-webkit-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+-moz-box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96);
+
+ 
+
+box-shadow: 0 8px 5px 6px rgba(41,30,247,0.96); width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين<a id="" target="_blank" type="button" style=" width: 6%; margin: 2px;" class=""><img src ="  " etclass="fr" style="width: 0px;margin-top: -147px;position: fixed;margin-left: -87px;"><img src ="https://w99w.net/sico/z1cctpk1r310.gif" etclass="fr" style="width: 37px;margin-top: -147px;position: fixed;margin-left: -258px;"><center>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="https://alodyni.000webhostapp.com/5.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$('.tc span[class="fa fl"]').addClass('fa-check'),$(`<link rel="stylesheet" href="https://cdn.rawgit.com/teamalmhia/type/fd0fac7e/Ashq-bdwy.css">`).insertBefore('body'),
+
+`).insertBefore('.nav-tabs');
+
+ 
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+ 
+
+$(".fl.ustat").css("width", "10px") 
+
+$(".fl.ustat").css("height", "10px") 
+
+$(".fl.ustat").css("border-radius", "10px") 
+
+$(".fl.ustat").css("position", "static") 
+
+$(".fl.ustat").css("margin-top", "10px") 
+
+$(".fl.ustat").css("margin-left", "0px") 
+
+ 
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+
+<link rel="stylesheet" href="ashq-bdwy.css">
+
+ 
+
+</style>`).insertBefore('body');
+
+ 
+
+$('.break.light').css({"background-image":"url(https://ashq-bdwy.com/ashq-bdwy.jpeg)"});
+
+ 
+
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+
+$('.fa.fa-user-plus').html(`تسجيل`);(document.getElementById("tbox").placeholder = "دردش معنا ");
+
+ 
+
+$(`<a href="Shortcut.html" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" ><i class="fa fa-reddit-alien"></i>&nbsp;اختصارات</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');var nopm = !0;var nonot = !0;
+
+ 
+
+ 
+
+ 
+
+$(`<center><div><marquee direction="right" width="99%" id="asim8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font class="a"> الـــملكــه</font> border: 9px double #eb0c3c;
+
+ 
+
+ ● · افضل سوبر  · ●<font class="a"> قريبــآ </font> ● · افضل زائر  · ●<font class="a"> قريبــآ </font> ● · افضل زائرة  · ●<font class="a"> قريبــآ </font> ● · مبدع الحائط · ●<font class="a"> قريبــآ </font> ● · مبدعة الحائط · ● <font class="a"> قريبــآ </font> ● · افضل تواجد · ●<font class="a"> قريبـآ </font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+
+ 
+
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+
+ 
+
+ 
+
+ 
+
+/*
+
+ 
+
+$("img[src='prv1.png?2']").attr("src","https://w99w.net/sico/z1cctpk1r310.gif");
+
+ 
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+ 
+
+$(`<div id="alodyni107"></div>
+
+ 
+
+ 
+
+ 
+
+<img class="fl" id="alodyni4"src=""width="100%" ></img><div>
+
+ 
+
+<div style=""><div width="150px" id="alodyni8"></div></div>
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+<marquee direction="right" width="54%" id="alodyni5" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" style="background: none;color: white;margin-left: 8.5%;">اللهم اهله علينا بالامن و الايمان،و السلامة و الاسلام، و العافية المجللة و الرزق الواسع، و دفع الاسقام، اللهم ارزقنا صيامه و قيامه و تلاوة القرآن فيه، و سلمه لنا، و تسلمه منا و سلمنا فيه</marquee></div>
+
+ 
+
+$(`
+
+ 
+
+`).insertAfter('div#l3');
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+$(function() {
+
+ 
+
+            
+
+ 
+
+                $("#alodyni8").typed({
+
+ 
+
+                  strings: [
+
+ 
+
+"رمضان كريم",
+
+ 
+
+ "وكل عام وانتم بالف خير" ,
+
+ 
+
+ "اوله رحمة",
+
+ 
+
+ "واوسطه مغفرة",
+
+ 
+
+ "واخرة عتقً من النار ",
+
+ 
+
+ ], 
+
+ 
+
+               
+
+ 
+
+                  loop: true,
+
+ 
+
+                  startDelay: 1e3,
+
+ 
+
+                  backDelay: 2e3
+
+ 
+
+                });
+
+ 
+
+              });
+
+ 
+
+$(`
+
+ 
+
+<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+
+ 
+
+<link rel="stylesheet" type="text/css" href="https://Ashq-bdwy.css">
+
+ 
+
+ 
+
+ 
+
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/droid-arabic-kufi" type="text/css"/>
+
+ 
+
+`).insertBefore('head title');
+
+ 
+
+$('ul.nav.nav-tabs.fl').appendTo('#alodyni107');
+
+ 
+
+ 
+
+ 
+
+(document.getElementById("tbox").placeholder = "اكتب رسالتك في شـات الملكة ");
+
+ 
+
+$('<button style=" margin-top:2px;margin-left:2px;" class="fa fa-phone call fl btn btn-success">&nbsp;&nbsp;&nbsp;&nbsp;</button>').insertAfter("button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary");
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ // $("nav  nav-tabs fl li").removeClass(".active");
+
+ 
+
+ 
+
+ 
+
+    
+
+ 
+
+$(`<iframe width="200" height="100" src="https://www.youtube.com/embed/MFMFVupioMY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`).insertBefore('label.label.label-primary.mini.fl');
+
+ 
+
+ 
+
+ 
+
+$(`<div class="fr borderg" style="margin-top: -68px;padding:2px;background-color:white;margin-right:4px;">فوازير رمضان: <a href="#">قريباً</a></div>`).insertAfter('label.label.label-primary.mini.fl');
+
+ 
+
+ 
+
+ 
+
+  
+
+ 
+
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="https://alodyni.000webhostapp.com/imgw/43-32.png" width="100%">`);
+
+ 
+
+ 
+
+ 
+
+/* var el = $(".el");
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ <div class="el">سبحان الله</div>
+
+ 
+
+<div class="el">الحمد لله</div>
+
+ 
+
+<div class="el">لا اله الا الله</div> 
+
+ 
+
+<div class="el">الله اكبر </div> 
+
+ 
+
+ 
+
+ 
+
+TweenMax.set(el,{autoAlpha:0,left:'0%',xPercent:"-50%"})
+
+ 
+
+var tl = new TimelineMax({repeat:-1});
+
+ 
+
+ 
+
+ 
+
+for(var i=0;i<el.length;i++){
+
+ 
+
+  E = el[i];
+
+ 
+
+  tl.to(E,0.5,{autoAlpha:1,left:'30%',ease:Power2.easeIn})
+
+ 
+
+   .to(E,3.5,{left:'70%',ease:Power0.easeNone})
+
+ 
+
+   .to(E,0.5,{left:'100%',autoAlpha:0,ease:Power2.easeOut})
+
+ 
+
+};
+
+ 
+
+*/
+
