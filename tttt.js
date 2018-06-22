1a7cde028d87846766a1ce5c0cb1dc9de163b385
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-06-22 16:57:59 +0300

    Create tttt.js

---
+++ tttt.js
@@ -0,0 +1,60 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+
+ 
+
+$("img[src='prv1.png?2']").attr("src","https://cf1.s3.souqcdn.com/item/2017/01/24/17/52/15/27/item_XL_17521527_23879918.jpg");
+
+ 
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · мυsɪc · ●&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>اغاني</p><audio controls loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+
+$(`<img src="ashq-bdwy.gif" class="fr" style="width:100%;margin-right: 2px;">
+
+$(`<center><div><div width="99.5%" style="background-image:url(https://n1w.net/a3s.jpg) ;color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >اهلاً وسهلاً بكم فِي شآت الملكه</font></div></div><center>`).insertBefore('#d2');
+
+ 
+
+<a href="sub.html" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-info">⠀الإشتراكات⠀</a>
+
+<a href="rules.html" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-danger">⠀القوانيـن⠀</a>  
+
+<a href="" style="border-radius: 3px 12px 11px 11px;width:110px;margin:3px;" class="fl mini  fa fa-heartbeat btn btn-info">⠀التطبيـق⠀</a>  
+
+`).insertBefore('.nav-tabs');
+
+ 
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+ 
+
+$(".fl.ustat").css("width", "10px") 
+
+$(".fl.ustat").css("height", "10px") 
+
+$(".fl.ustat").css("border-radius", "10px") 
+
+$(".fl.ustat").css("position", "static") 
+
+$(".fl.ustat").css("margin-top", "10px") 
+
+$(".fl.ustat").css("margin-left", "0px") 
+
+ 
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+
+<link rel="stylesheet" href="ashq-bdwy.css">
+
+ 
+
+</style>`).insertBefore('body');
+
+ 
+
+$('.break.light').css({"background-image":"url(https://ashq-bdwy.com/ashq-bdwy.jpeg)"});
+
+ 
+
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+
