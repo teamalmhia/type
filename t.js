dd9efb51747772b2594b85915d5a9f40b61d44ed
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-06-25 17:52:26 +0300

    Create t.js

---
+++ t.js
@@ -0,0 +1,20 @@
+
+<html> 
+ 
+<head> 
+ <title>Background and text color</title> 
+ <style> 
+  .clr_cls { 
+   background-color: red; border: 3px double #f04713;
+border-radius: 38px 0;
+  } 
+  </style>
+</head> 
+ 
+<body> 
+ <div class="clr_cls">
+<center><div><marquee direction="right" width="99%" id="n1w8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;●・🌹🌹 اهلا وسهلا بكم في شات 🌹🌹・●  <font style="background-color: #fff;border-radius: 30px 40px 40px 30px;" color="#0000ff"</font>🌟💝
+
+الـــملكــه💝🌟</font> ●• · افضل سوبر  · •●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#ff00ff"> قريبــآ </font> ● · افضل زائر  · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · افضل زائرة  · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · مبدع الحائط · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;" color="#0000ff"> قريبــآ </font> •● · مبدعة الحائط · •● <font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · افضل تواجد · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#0000ff"> قريبـآ </font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+
+</marquee></div>
