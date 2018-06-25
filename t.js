e2db40b4db06cc748cd4596c51f98f44682a7bda
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-06-26 00:46:51 +0300

    t.js

--- t.js
+++ t.js
@@ -1,19 +1,184 @@
+if(location.protocol!='https:'){location.protocol='https:';}
 
-$(`<center>
- <title>Background and text color</title> 
- <style> 
-  .clr_cls { 
-   background-color: red; border: 3px double #f04713;
-border-radius: 38px 0;
-  } 
-  </style>
-</head> 
- 
+$("img[src='prv1.png?2']").attr("src","https://cf1.s3.souqcdn.com/item/2017/01/24/17/52/15/27/item_XL_17521527_23879918.jpg");
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● ·( إضغط هنا إرح سمعّك مع القرآن )· ● &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>هزآع البلوشي</p><audio controls loop="" play=""><source src="https://www.jawalhost.com/quran.mp3"></audio></div>
+
+<center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+
+ 
+
+ 
+
+ 
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · мυsɪc · ●&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>اغاني</p><audio controls loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+
+ 
+
+$(`<center><div><div width="99.5%" style="background-image:url(https://raw.githubusercontent.com/teamalmhia/typed/master/20180622_185535.gif) ;color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >اهلاً وسهلاً بكم فِي شآت الملكه</font></div></div><center>`).insertBefore('#d2');
+
+ 
+
+ 
+
+$(`<img src="https://raw.githubusercontent.com/teamalmhia/typed/master/20180625_002639.gif" class="fr" style="width:100%;margin-right: 2px;">
+
+<a href="sub.html" style="border: 5px double #e81a7d;
+
+border-radius: 0 81px;width:110px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-info">⠀الإشتراكات⠀</a>
+
+<a href="rules.html" style="border: 5px double #e8e52c;
+
+border-radius: 0 81px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-danger">⠀القوانيـن⠀</a>  
+
+<a href="" style="border: 5px double black;
+
+border-radius: 0 81px;width:110px;margin:3px;" class="fl mini  fa fa-heartbeat btn btn-info">⠀التطبيـق⠀</a>  
+
+ 
+
+`).insertBefore('.nav-tabs');
+
+ 
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+ 
+
+$(".fl.ustat").css("width", "10px") 
+
+$(".fl.ustat").css("height", "10px") 
+
+$(".fl.ustat").css("border-radius", "10px") 
+
+$(".fl.ustat").css("position", "static") 
+
+$(".fl.ustat").css("margin-top", "10px") 
+
+$(".fl.ustat").css("margin-left", "0px") 
+
+ 
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+
+<link rel="stylesheet" href="https://iraqiachats.com/iraqna1.css">
+
+ 
+
+</style>`).insertBefore('body');
+
+ 
+
+$('.break.light').css({"background-image":"url(https://ashq-bdwy.com/ashq-bdwy.jpeg)"});
+
+ 
+
+$( "button.btn.btn-primary").css("border-radius:","24px 24px 24px 24px")
+
+ 
+
+ 
+
+ 
+
+$(document).ready($("div.fr.borderg").css("display","none"));
+
+ 
+
+$("#wall .tablebox").insertBefore('.d2bc');
+
+nopm=true;
+
+ 
+
+ 
+
+ 
+
+emopop=function (eid) {
+
+ 
+
+  var emo = $(eid)
+
+ 
+
+  emo.popover({
+
+ 
+
+    html: true,
+
+ 
+
+    content: function () {
+
+ 
+
+      var emosh = $("<div style='max-width:250px;'    class='break corner'></div>");
+
+ 
+
+      $.each(emos, function (i, e) {
+
+ 
+
+        emosh.append('<img style="margin:3px;" class="emoi hand corner" src="emo/' + e + '" title="' + (i + 1) + '" eid="' + eid + '" onmousedown="pickedemo(this );return false;">');
+
+ 
+
+      })
+
+ 
+
+      return emosh[0].outerHTML;
+
+ 
+
+    },
+
+ 
+
+    title: ""
+
+ 
+
+  });
+
+ 
+
+}
+
+$(`<style>
+
+  .clr_cls { 
+
+   background-color: red; border :2px double; border-radius : 15px  0 15px  0; 
+
+   color: #fff ; 
+
+  } 
+
+ </style> 
+
+ 
+
+ 
+
 <body> 
- <div class="clr_cls">
-<center><div><marquee direction="right" width="99%" id="n1w8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;●・🌹🌹 اهلا وسهلا بكم في شات 🌹🌹・●  <font style="background-color: #fff;border-radius: 30px 40px 40px 30px;" color="#0000ff"</font>🌟💝
 
-الـــملكــه💝🌟</font> ●• · افضل سوبر  · •●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#ff00ff"> قريبــآ </font> ● · افضل زائر  · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · افضل زائرة  · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · مبدع الحائط · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;" color="#0000ff"> قريبــآ </font> •● · مبدعة الحائط · •● <font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · افضل تواجد · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#0000ff"> قريبـآ </font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+ <div class="clr_cls"> 
 
-</marquee></div>
-<center>`).insertBefore('div#tlogins .lonline.light.break'); 
+ 
+
+  <center><div><marquee direction="right" width="99%" id="n1w8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><b>
+
+ 
+
+  &nbsp;●・🌹🌹 اهلا وسهلا بكم في شات 🌹🌹・●  <font style="background-color: #fff;border-radius: 30px 40px 40px 30px;" color="#0000ff"</font>🌟💝
+
+الـــملكــه💝🌟</font> 👑●• · افضل سوبر  · •●👑<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#ff00"> قريبــآ </font>🎩 ● · افضل زائر  · ●🎩<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#ff00ff"> قريبــآ </font> ● · افضل زائرة  · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CADA"> قريبــآ </font> ● · مبدع الحائط · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;" color="#0000ff"> قريبــآ </font> •● · مبدعة الحائط · •● <font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#5CAD9A"> قريبــآ </font> ● · افضل تواجد · ●<font style="background-color: #fff;border-radius: 15px 15px 15px 15px;"color="#000"> قريبـآ </font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+
+</marquee></div><center>`). insertAfter('div#l3');
+
