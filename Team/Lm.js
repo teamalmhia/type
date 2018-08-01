b01b8eef4ae258ffe484aff4ea5d8f7c26391ad4
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-08-01 17:18:33 +0300

    Create Lm.js

---
+++ Team/Lm.js
@@ -0,0 +1,23 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<img src="https://www.lmshh.com/sico/z1cija1k9b10.gif" class="fr" style="width:100%;margin-right:0px;">
+
+<div id="as8"><center><a id="as7" href="contact.html" target="_blank" type="button" class="btn btn-primary btn-sm">اتصل بنا</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">التطبيق</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">اشتراكات</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">اختصارات</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">زخرفة</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">القوانين</a><a id="as7" href="#" target="_blank" type="button" class="btn btn-primary btn-sm">المراقبين</a><a id="as2" href="#" target="_blank" type="button" class="fa fa-viadeo btn btn-primary btn-sm"> المدونة</a><a id="as1" href="#" target="_blank" type="button" class="fa fa-viadeo btn btn-primary btn-sm"> المنتدى</a><a id="as9" href="" target="_blank" type="button" class="fa fa-viadeo btn btn-primary btn-sm"> غالي</a><a id="as2" href="#" target="_blank" type="button" class="fa fa-viadeo btn btn-primary btn-sm"> المدونة</a><a id="as1" href="#" target="_blank" type="button" class="fa fa-viadeo btn btn-primary btn-sm"> المنتدى</a>
+
+<center></center></a></center></div>
+
+`).insertBefore('.nav-tabs');
+
+$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● · مـوسـيـقـئ · ●  &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>شات لمسه تصميم تيم</p><audio controls loop="" play=""><source src="https://d.top4top.net/m_902ut94b1.mp3" "></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$('.break.light').css({"background-image":"url()"});
+
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" type="text/css" href="https://rawgit.com/teamalmhia/type/master/Team/lm.css">
+</style>`).insertBefore('body');
+$('.fa.fa-user-plus').html(`تسجيل`),
+
+$.getScript('https://iraqiachats.com/\u0634\u0627\u062A\u0639\u0631\u0627\u0642\u0627\u0644\u0631\u0648\u0645\u0627\u0646\u0633\u064A\u0629.js');$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="galy13" src="https://iqr30.com/IQ/5.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$('.tc span[class="fa fl"]').addClass('fa-check'),document.getElementById('tbox').placeholder='\u0639\u0645\u064A \u0639\u0648\u0641 \u0627\u0644\u0633\u0637\u062D \u0648\u062F\u0631\u062F\u0634 \u0648\u064A\u0627\u0646\u0627',$(`<a id="galychat20" href="Shortcut.html" target="_blank"class="border" ><i class="fa fa-reddit-alien"></i>&nbsp;اختصارات</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+$(`<center><div><marquee direction="right" width="99%" id="galy5" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;آهلا وسهلا بكم في شات <font class="a"> لمسه للجوال </font> ● · افضل سوبر لهذا الأسبوع  · ●<font class="a"> قريبـــــاً </font> ● · افضل زائر لهذا الأسبوع  · ●<font class="a"> قريبـــــاً</font> ● · افضل زائرة لهذا الأسبوع  · ●<font class="a"> قريبـــــاً </font> ● · مبدع الحائط لهذا الأسبوع · ●<font class="a"> قريبـــــاً</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="a"> قريبـــــاً</font> ● · افضل تواجد لهذا الأسبوع · ●<font class="a"> قريبـــــاً</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');$(".corner.fa.fa-user.label.label-primary.fr.uc").css("border-radius","15px")
+$(".fr.co").css("border-radius","15px")
