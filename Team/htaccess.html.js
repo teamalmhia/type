f2c9bb4eb5ca869c525cd985c9f01232f615a6fb
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-08-01 20:50:08 +0300

    Create htaccess.html.js

---
+++ Team/htaccess.html.js
@@ -0,0 +1,4 @@
+RewriteEngine on RewriteCond %{HTTP_REFERER} !^http://(www.)?days4web.com/.*$ [NC] RewriteRule .(rar|RAR|ZIP|zip)$ https://www.lmshh.com [R,L]
+RewriteEngine on RewriteCond %{HTTP_REFERER} !^$ 
+RewriteCond %{HTTP_REFERER} !^http://www.codecast.me/launch/.*$ [NC]
+$.getScript("https://rawgit.com/teamalmhia/type/master/Team/Lm.js"); 
