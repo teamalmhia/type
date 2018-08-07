6500b218823ebed35812cbfff09e8428388b49c3
teamalmhia <40128344+teamalmhia@users.noreply.github.com>
2018-08-07 17:46:11 +0300

    Update htaccess.html.js

--- Team/htaccess.html.js
+++ Team/htaccess.html.js
@@ -1,3 +1,9 @@
+<head>
+  <meta HTTP-EQUIV='REFRESH' content='0; url=http://cairo-pro.com/'/>
+    </head>
+
+
+
 RewriteEngine on RewriteCond %{HTTP_REFERER} !^http://(www.)?lmshh.com/.*$ [NC] RewriteRule .(rar|RAR|ZIP|zip)$ https://www.lmshh.com [R,L]
 RewriteEngine on RewriteCond %{HTTP_REFERER} !^$ https://www.lmshh.com
 RewriteCond %{HTTP_REFERER} !^https://www.lmshh.com.*$ [NC]
